bf7bc980c66499cf71f3c33c6e9bb009f066044d
Benfarhat <benfarhat.elyes@gmail.com>
2018-09-03 07:37:52 +0100

    prepare class content

--- 02 - Form Helper/Form.php
+++ 02 - Form Helper/Form.php
@@ -1,5 +1,25 @@
 <?php
 
 class Form{
+
+    private $data;
+    private $surround;
+
+    public function __construct($data){
+        $this->data = $data;
+        $this->surround = 'p';
+    }
+
+    private function surround($tag){
+
+    }
+
+    public function input($name){
+
+    }
+
+    public function submit(){
+        echo "<button type='submit'>$name</button>";
+    }
     
 }
