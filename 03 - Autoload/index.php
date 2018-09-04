1e538f95659484241da028041c2f691bd9934e43
Benfarhat <benfarhat.elyes@gmail.com>
2018-09-05 00:21:01 +0100

    Autoloader and namespace

--- 03 - Autoload/index.php
+++ 03 - Autoload/index.php
@@ -4,11 +4,20 @@ namespace NS;
 use NS\Autoloader;
 use NS\rep\a\A;
 use NS\rep\b\B;
+use NS\rep\b\A as C;
 
 require 'Autoloader.php';
 
 Autoloader::register();
-
+/*
+Pour infos, les namespace permettent également d'éviter les conflits de noms de class
+Donc avec cela on peut utiliser plusieurs foit la classe A dans plusieurs Namespace différents
+Notez qu'avant cela, on donnait des noms de classes très longues
+*/
 echo A::msg();
 echo "<hr>";
 echo B::msg();
+echo "<hr>";
+echo C::msg();
+echo "<hr>";
+echo rep\b\A::msg();
---
+++ 03 - Autoload/rep/b/A.php
@@ -0,0 +1,8 @@
+<?php
+namespace NS\rep\b;
+
+class A{
+    static function msg(){
+        return "msg from another A";
+    }
+}
