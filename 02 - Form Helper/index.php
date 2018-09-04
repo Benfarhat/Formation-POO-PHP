3bbfe651924acfb70a8e4b422304bf090c6ec611
Benfarhat <benfarhat.elyes@gmail.com>
2018-09-04 21:57:08 +0100

    autoload with spel_autoload_register

--- 02 - Form Helper/index.php
+++ 02 - Form Helper/index.php
@@ -1,6 +1,32 @@
 <?php
 
-require('Form.php');
+// require('Form.php');
+
+/*
+function __autoload($class){ // Deprecated
+    var_dump('autoload appelé');
+    die();
+}
+*/
+
+spl_autoload_register( function ($class) {
+    include $class . '.php';
+});
+
+/*
+
+ou pour appeler une fonction
+
+spl_autoload_register( 'nom_de_la_fonction' );
+
+ou pour appeler une fonction static dans une classe
+
+require 'chemin vers la classe .. php';
+spl_autoload_register( array( 'Nom de la classe', 'nom de la fonction))
+
+
+
+*/
 ?>
 <form action="#" method="post">
 <?php
@@ -20,6 +46,7 @@ $form = new Form($_POST);
 
 echo $form->input('username');
 echo $form->input('password');
+echo $form->input('email');
 echo $form->submit();
 ?>
 </form>
