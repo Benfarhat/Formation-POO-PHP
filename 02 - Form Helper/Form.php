524f0d05e1df060148497a467f18d9ae4bdc952a
Benfarhat <benfarhat.elyes@gmail.com>
2018-09-04 01:07:30 +0100

    Some PHP Docs

--- 02 - Form Helper/Form.php
+++ 02 - Form Helper/Form.php
@@ -1,36 +1,68 @@
 <?php
 
-class Form{
+/**
+ * Class Form 
+ * Permet de générer un formulaire rapidement et simplement
+ */
+class Form
+{
 
+    /**
+     * Données utilisées par le formulaire
+     *
+     * @var array
+     */
     private $data;
+
+    /**
+     * Tag pour entourer les champs
+     *
+     * @var string
+     */
     private $surround;
 
-    public function __construct($data = []){
+    /**
+     * Fonction __construct
+     *
+     * @param array $data
+     */
+    public function __construct($data = [])
+    {
         $this->data = $data;
         $this->surround = 'p';
-   
+
     }
 
-    private function surround($html){
+    /**
+     * Entoure le code avec un tag
+     *
+     * @param string $html le code à entourer
+     * @return void
+     */
+    private function surround($html)
+    {
         return "<{$this->surround}>$html</{$this->surround}>";
     }
 
-    private function getValue($index){
-        return isset($this->data[$index]) ? $this->data[$index] : null ;
+    private function getValue($index)
+    {
+        return isset($this->data[$index]) ? $this->data[$index] : null;
     }
 
-    public function input($name){
+    public function input($name)
+    {
         return $this->surround('<input type="text" name ="' . $name . '" value="' . $this->getValue($name) . '">');
     }
 
-    public function submit(){
+    public function submit()
+    {
         return $this->surround('<button type="submit">Envoyer</button>');
     }
-    
+
 
     /**
      * Get the value of surround
-     */ 
+     */
     public function getSurround()
     {
         return $this->surround;
@@ -40,7 +72,7 @@ class Form{
      * Set the value of surround
      *
      * @return  self
-     */ 
+     */
     public function setSurround($surround)
     {
         $this->surround = $surround;
