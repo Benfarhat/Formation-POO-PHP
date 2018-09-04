d5ba08d6705a0552886f7293d8da35db0f63ea8b
Benfarhat <benfarhat.elyes@gmail.com>
2018-09-04 01:00:56 +0100

    Initial commit

--- 02 - Form Helper/Form.php
+++ 02 - Form Helper/Form.php
@@ -5,21 +5,46 @@ class Form{
     private $data;
     private $surround;
 
-    public function __construct($data){
+    public function __construct($data = []){
         $this->data = $data;
         $this->surround = 'p';
+   
     }
 
-    private function surround($tag){
+    private function surround($html){
+        return "<{$this->surround}>$html</{$this->surround}>";
+    }
 
+    private function getValue($index){
+        return isset($this->data[$index]) ? $this->data[$index] : null ;
     }
 
     public function input($name){
-
+        return $this->surround('<input type="text" name ="' . $name . '" value="' . $this->getValue($name) . '">');
     }
 
     public function submit(){
-        echo "<button type='submit'>$name</button>";
+        return $this->surround('<button type="submit">Envoyer</button>');
     }
     
+
+    /**
+     * Get the value of surround
+     */ 
+    public function getSurround()
+    {
+        return $this->surround;
+    }
+
+    /**
+     * Set the value of surround
+     *
+     * @return  self
+     */ 
+    public function setSurround($surround)
+    {
+        $this->surround = $surround;
+
+        return $this;
+    }
 }
--- 02 - Form Helper/index.php
+++ 02 - Form Helper/index.php
@@ -1,4 +1,25 @@
 <?php
 
 require('Form.php');
+?>
+<form action="#" method="post">
+<?php
+$form = new Form([
+    'username' => 'Rogey'
+    ]);
 
+echo $form->input('username');
+echo $form->input('password');
+echo $form->submit();
+?>
+</form>
+<form action="#" method="post">
+
+<?php
+$form = new Form($_POST);
+
+echo $form->input('username');
+echo $form->input('password');
+echo $form->submit();
+?>
+</form>
